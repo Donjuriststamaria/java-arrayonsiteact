ee1cd42859f91728875a5a4ff8e10e3a5643b1e8
Donjuriststamaria <sdonjurist@gmail.com>
2026-01-14 13:45:58 +0800

    Updated the main file by adding methods

--- arrayonsiteact.java
+++ arrayonsiteact.java
@@ -1,45 +1,61 @@
 public class Main {
-    public static void main (String[] args) {
 
-        System.out.println("Numbers:");
-        
+    public static void main(String[] args) {
         int[] number = {10, 20, 5, 30, 15, 25, 40, 2, 8, 12};
-        int sum=0;
-        
-        //Number 1
-        for (int i = 0; i < number.length;i++){
-        System.out.print(number[i] + " ");             
-        }
-        
-        
-        //Number 2        
-        System.out.println("\nNumbers in Reverse:");
-        for (int i = 9; i >= 0;i--){
-        System.out.print(number[i] + " ");             
-        }
 
-        //Number 3
-        for (int i = 0; i < number.length;i++){
-        sum += number[i]; }
-        System.out.print("\nSum of Numbers: " +sum);  
-        
-        //Number 4
-        System.out.println("\nNumbers less than 15:");  
-        for (int i = 0; i < number.length;i++){
-        if (number[i]<15) {
-        System.out.print(number[i] + " ");
-    
-        } 
+        printNumbers(number);          // 1
+        printReverse(number);          // 2
+        printSum(number);              // 3
+        printLessThan15(number);       // 4
+        printAboveAverage(number);     // 5
+    }
+
+    // Method 1: Print numbers
+    static void printNumbers(int[] number) {
+        System.out.println("Numbers:");
+        for (int i = 0; i < number.length; i++) {
+            System.out.print(number[i] + " ");
         }
-        
-        //Number 5
-        System.out.println("\nNumbers above average (16.7):");  
-        for (int i = 0; i < number.length;i++){
-        if (number[i]>16.7) {
-        System.out.print(number[i] + " ");
-    
-        } 
+        System.out.println();
+    }
+
+    // Method 2: Print numbers in reverse
+    static void printReverse(int[] number) {
+        System.out.println("Numbers in Reverse:");
+        for (int i = number.length - 1; i >= 0; i--) {
+            System.out.print(number[i] + " ");
         }
-        
-}
+        System.out.println();
+    }
+
+    // Method 3: Print sum
+    static void printSum(int[] number) {
+        int sum = 0;
+        for (int i = 0; i < number.length; i++) {
+            sum += number[i];
+        }
+        System.out.println("Sum of Numbers: " + sum);
+    }
+
+    // Method 4: Print numbers less than 15
+    static void printLessThan15(int[] number) {
+        System.out.println("Numbers less than 15:");
+        for (int i = 0; i < number.length; i++) {
+            if (number[i] < 15) {
+                System.out.print(number[i] + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    // Method 5: Print numbers above average
+    static void printAboveAverage(int[] number) {
+        System.out.println("Numbers above average (16.7):");
+        for (int i = 0; i < number.length; i++) {
+            if (number[i] > 16.7) {
+                System.out.print(number[i] + " ");
+            }
+        }
+        System.out.println();
+    }
 }
